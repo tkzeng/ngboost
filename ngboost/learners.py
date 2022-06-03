9e6d6ce22a3022df1960aec090878c6b158ec868
Tony Zeng <tkzeng@stanford.edu>
2022-06-03 00:30:46 -0700

    Update learners.py

--- ngboost/learners.py
+++ ngboost/learners.py
@@ -1,14 +1,22 @@
 from sklearn.linear_model import Ridge
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import HistGradientBoostingRegressor
 
-default_tree_learner = DecisionTreeRegressor(
-    criterion="friedman_mse",
-    min_samples_split=2,
-    min_samples_leaf=1,
-    min_weight_fraction_leaf=0.0,
+#default_tree_learner = DecisionTreeRegressor(
+#    criterion="friedman_mse",
+#    min_samples_split=2,
+#    min_samples_leaf=1,
+#    min_weight_fraction_leaf=0.0,
+#    max_depth=3,
+#    splitter="best",
+#    random_state=None,
+#)
+
+# better handling of missing data
+default_tree_learner = HistGradientBoostingRegressor(
+    max_iter=1,
     max_depth=3,
-    splitter="best",
-    random_state=None,
+    min_samples_leaf=1
 )
 
 default_linear_learner = Ridge(alpha=0.0, random_state=None)
